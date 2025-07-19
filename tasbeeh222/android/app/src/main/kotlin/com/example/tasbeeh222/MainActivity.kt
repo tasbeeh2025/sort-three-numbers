e80e008a0ed7cdee708971251a65ecd68e3dd8db
tasbeeh2025 <mtasbeeh53@gmail.com>
2025-07-19 19:42:01 +0300

    Add files via upload

---
+++ tasbeeh222/android/app/src/main/kotlin/com/example/tasbeeh222/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tasbeeh222
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ tasbeeh222/lib/sort three numbers.dart
@@ -0,0 +1,32 @@
+int a,b,c,d;
+a=int.parse(stdin.readLineSync()!);
+b=int.parse(stdin.readLineSync()!);
+c=int.parse(stdin.readLineSync()!);
+if(a<b&&a<c){
+  if(b<c){
+    print("$a ,$b ,$c");
+  }
+  if(c<b){
+    print("$a ,$c ,$b");
+  }
+}
+else if(b<a&&b<c){
+  if(a<c){
+    print("$b ,$a ,$c");
+  }
+  if(c<a){
+    print("$b ,$c ,$a");
+  }
+
+}
+else if(c<a&&c<b){
+  if(b<a){
+    print("$c ,$b ,$a");
+  }
+  if(a<b){
+    print("$c ,$a ,$b");
+  }
+}
+else{
+  print("there are similar numbers");
+}
